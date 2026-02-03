f4674e33c4beab070fc62790b4db5026f4037957
sushmasunitha <154217964+sushmasunitha@users.noreply.github.com>
2026-02-03 16:34:06 +0530

    Update day2_check1.py

--- day2_check1.py
+++ day2_check1.py
@@ -1,14 +1,8 @@
-# Ask for the user's name
-name = input("What is your name? ")
 
-# Ask for the user's current age
+name = input("What is your name? ")
 current_age = input("How old are you right now? ")
 
-# Convert age to an integer
 current_age = int(current_age)
-
-# Calculate age in 2030 (2026 → 2030 = 4 years)
 age_in_2030 = current_age + 4
-
-# Output the result
 print(f"Hey {name}, you will be {age_in_2030} years old in 2030!")
+
