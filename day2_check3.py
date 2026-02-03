3edd9ab41b674f73fc03753bc46fa60a9d85733b
sushmasunitha <154217964+sushmasunitha@users.noreply.github.com>
2026-02-03 11:28:08 +0530

    Add files via upload

---
+++ day2_check3.py
@@ -0,0 +1,10 @@
+item_name = "Laptop"      # String
+quantity = 2              # Integer
+price = 499.99            # Float
+in_stock = True            # Boolean
+
+
+print("Item:", item_name, "Qty:", quantity, "Price:", price, "Available:", in_stock)
+
+total_cost = quantity * price
+print("Total Cost:", total_cost)
