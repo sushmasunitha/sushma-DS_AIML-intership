34823116871d9edbebb0de82f4849ae5a0e1c52f
Sushma M <154217964+sushmasunitha@users.noreply.github.com>
2026-02-04 14:34:40 +0530

    Add files via upload

---
+++ day3_check1.py
@@ -0,0 +1,8 @@
+inventory = ["Apples", "Bananas", "Carrots", "Dates"]
+print("Current inventory:", inventory)
+
+inventory.append("Eggs")
+inventory.remove("Bananas")
+inventory.sort()
+
+print("Final inventory:", inventory)
